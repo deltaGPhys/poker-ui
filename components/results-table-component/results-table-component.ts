a9c2574cc690ef4fffc352051bc69aa0b7bdcda1
deltaGPhys <cty.jgates@gmail.com>
2025-07-26 12:35:31 -0400

    rank fix

--- components/results-table-component/results-table-component.ts
+++ components/results-table-component/results-table-component.ts
@@ -16,6 +16,7 @@ export class ResultsTableComponent {
 
   constructor(private playerService: PlayerService) {
     this.playerService.players.subscribe(p =>
-      this.players = p.sort((a,b) => (a.s.state === "COMPLETE" && b.s.state === "COMPLETE") ? a.s.finalRank < b.s.finalRank ? 1 : -1 : 1));
+      this.players = p.filter( p => p.s.state === "COMPLETE")
+        .sort((a,b) => (a.s.state === "COMPLETE" && b.s.state === "COMPLETE") ? a.s.finalRank > b.s.finalRank ? 1 : -1 : -1));
   }
 }
